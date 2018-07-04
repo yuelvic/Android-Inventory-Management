aa9aae73babe8931d098d1aff22a67859458ba89
Emmanuel Victor Garcia <emmanuel.garcia@xurpas.com>
2018-07-04 10:55:50 +0800

    Initial commit

---
+++ app/src/main/java/io/chaman/im/MainActivity.kt
@@ -0,0 +1,19 @@
+package io.chaman.im
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.chaman.im.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/io/chaman/im/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package io.chaman.im.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
